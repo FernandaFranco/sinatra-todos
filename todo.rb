5305ca199d7d1c10bd053e3c6465968bf31310c3
Fernanda Franco <francodias.fernanda@gmail.com>
2017-05-11 18:56:31 -0400

    added marking all as completed functionality

--- todo.rb
+++ todo.rb
@@ -141,3 +141,15 @@ post "/lists/:list_id/todos/:todo_id" do
   session[:success] = "The todo has been updated."
   redirect "/lists/#{list_id}"
 end
+
+# Mark all items completed
+post "/lists/:list_id/complete_all" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  @list[:todos].each do |todo|
+    todo[:completed] = true
+  end
+
+  session[:success] = "All todos have been completed."
+  redirect "/lists/#{@list_id}"
+end
